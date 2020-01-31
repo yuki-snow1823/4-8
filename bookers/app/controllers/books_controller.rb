615303284704d394ff571f40676a2a36a1ef8cca
yuki-snow1823 <magic.iroha@gmail.com>
2020-01-31 09:11:40 +0900

    [fix]4-8

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -25,10 +25,16 @@ class BooksController < ApplicationController
   # ストロングパラメーターを使用詳細は下。新しいものの中に何を入れるのか？タイトルとボディ
     book = Book.new(book_params)
   # 投稿されたらDBへ保存する
-    book.save
+    if book.save
+     redirect_to show_book_path(book.id)  
   # トップ画面へリダイレクト→それ自身の詳細画面へリダイレクトしてあげる_pathで[:id]とかの代用ができている
   # 多分名前付きルートで[book]を指定した意味がここにある
-    redirect_to show_book_path(book.id) 
+    else
+      # 書き込み失敗
+      render action: :index
+      # redirect_to books_path
+      # これだとダメ？うまくいった　renderのこと調べる　エラーが表示されない…
+    end
   end
 
   def show
@@ -39,8 +45,11 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to show_book_path(book.id)
     # ここら辺何をしてるのか復習する
+    if book.update(book_params)
+      flash[:success] = "Book was successfully updated."
+      redirect_to show_book_path(book.id)
+    end
   end
 
   private
