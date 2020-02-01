605a469b5cc29ee94b243468051274bd9c005b1f
yuki-snow1823 <magic.iroha@gmail.com>
2020-02-01 09:38:30 +0900

    [fix]4-8

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -15,23 +15,25 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to books_path
     # 一覧画面へリダイレクトしたい
   end
 
   def create
   # ストロングパラメーターを使用詳細は下。新しいものの中に何を入れるのか？タイトルとボディ
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
   # 投稿されたらDBへ保存する
-    if book.save
-     redirect_to show_book_path(book.id)  
+    if @book.save
+     redirect_to show_book_path(@book.id)  
   # トップ画面へリダイレクト→それ自身の詳細画面へリダイレクトしてあげる_pathで[:id]とかの代用ができている
   # 多分名前付きルートで[book]を指定した意味がここにある
     else
       # 書き込み失敗
+      @books = Book.all
       render action: :index
+      # これだと一覧させるメソッドのせいでうまくいかない、空だから
       # redirect_to books_path
       # これだとダメ？うまくいった　renderのこと調べる　エラーが表示されない…
     end
--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -11,8 +11,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'homes#top'
   get 'books' => 'books#index', as:'index_book'
+  
   post 'books' => 'books#create'
   #投稿されたとき、createメソッド使う
+
   get 'books/:id' => 'books#show' , as: 'show_book'
   # ここが分かっていません
 end
